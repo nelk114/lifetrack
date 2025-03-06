e0028bf9be75ef83eb290b167e7a175f7b6e0e70
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-06 14:18:03 +0000

    Add Habit Model
    Only tracking start date w/ Date resolution for now
    (corresponding to the name, if despite our ER design).
    We could always increase the res later if necessary

--- lifetrack/models.py
+++ lifetrack/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 class UserProfile(models.Model):
 	user=models.OneToOneField(User,on_delete=models.CASCADE)
@@ -11,5 +12,12 @@ class HabitList(models.Model):
 	user=models.ForeignKey(User,on_delete=models.CASCADE)
 	name=models.CharField(max_length=128)
 	freq=models.CharField(max_length=128)	#Using strings as an enum substitute; we only support predefined frequencies
+	def __str__(σ):
+		return σ.name
+
+class Habit(models.Model):
+	list=models.ForeignKey(HabitList,on_delete=models.CASCADE)
+	name=models.CharField(max_length=128)
+	sdate=models.DateField(default=date.today)
 	def __str__(σ):
 		return σ.name
