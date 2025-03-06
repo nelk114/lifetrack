2d06bdc6a4cd057637ec559771d0d9ab386832d6
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-06 14:04:12 +0000

    Add UserProfile model
    The actual Django User model already provides username & password functionality
    so we needn't duplicate that.
    
    Technically a deviation from our ER design
    but justified in this case
    (& closely matches what we had in Rango)

--- lifetrack/models.py
+++ lifetrack/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+	user=models.OneToOneField(User,on_delete=models.CASCADE)
+	name=models.CharField(max_length=128)
+	def __str__(σ):
+		return σ.user.username
