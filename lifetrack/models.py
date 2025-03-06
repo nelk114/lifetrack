f5c9494e135615b415c394817145fd37633ee670
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-06 14:09:38 +0000

    Add HabitList model
    ‘List’ is too generic a name & risks stepping on some standard library
    (even if Python libs are typically named in lowercase,
    can't be too catious)

--- lifetrack/models.py
+++ lifetrack/models.py
@@ -6,3 +6,10 @@ class UserProfile(models.Model):
 	name=models.CharField(max_length=128)
 	def __str__(σ):
 		return σ.user.username
+
+class HabitList(models.Model):
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	name=models.CharField(max_length=128)
+	freq=models.CharField(max_length=128)	#Using strings as an enum substitute; we only support predefined frequencies
+	def __str__(σ):
+		return σ.name
