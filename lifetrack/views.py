31b456308fc4d4e6e52afa8158caf71c259c5044
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-14 12:59:18 +0000

    Add proper Login view
    Including Sign‐up functionality
    because we forgot to have a separate URL for that
    and also the hack(?) is kinda neat.
    
    Template to follow

--- lifetrack/views.py
+++ lifetrack/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from django.http import HttpResponse
+from django.contrib.auth import authenticate as auth,login as li,logout
+from lifetrack.forms import UserForm
 
 def index(r):
 	return render(r,'lifetrack/index.html')
 
 def login(r):
-	return HttpResponse(f'Stub. (login)')
+	reg,fail=False,False;status=200;uf=UserForm()
+	if r.method=='POST':
+		if r.POST.get('form')=='login':
+			n=r.POST.get('username');p=r.POST.get('password')
+			l=auth(username=n,password=p)
+			if l:
+				if l.is_active:li(r,l);return redirect(reverse('lifetrack:index'))
+				else:print(f'Disabled account: {n}');fail,status='disabled',401
+			else:print(f'Invalid login details: {n}, {p}');fail,status='login',401
+		elif r.POST.get('form')=='signup':
+			uf=UserForm(r.POST)
+			if uf.is_valid():
+				l=uf.save();l.set_password(l.password);l.save()
+				reg=True
+			else:print(uf.errors,pf.errors)
+		else:print('Unsupported form submission')
+	return render(r,'lifetrack/login.html',context={'user_form':uf,'registered':reg,'fail':fail},status=status)
 
 def lists(r):
 	return HttpResponse(f'Stub. (lists)')
@@ -21,5 +40,3 @@ def addhabit(r):
 
 def edithabit(r):
 	return HttpResponse(f'Stub. (edithabit)')
-
-
