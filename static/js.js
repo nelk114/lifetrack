e945721329f9844c8f9a9eb092f0600f4224c2c9
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-21 11:32:25 +0000

    Extract JS to separate file
    Soon(?) we can make a separate template
    and get all this copy–paste mess
    somewhat under control.

---
+++ static/js.js
@@ -0,0 +1,13 @@
+function showReg(){document.getElementById("user_form").style.display="block";}
+function tickBox(e){
+	cb=e.target;dt=cb.parentElement.id;hb=cb.parentElement.parentElement.parentElement.id;ls=cb.parentElement.parentElement.parentElement.parentElement.id;
+	csrf=document.getElementById("csrf").firstChild.value;
+	post="dt="+encodeURIComponent(dt)+"&hb="+encodeURIComponent(hb)+"&ls="+encodeURIComponent(ls);
+	//alert(post);
+	var xhr=new XMLHttpRequest();
+	xhr.onreadystatechange=function(){if(this.readyState==4&&this.status==200)alert(xhr.responseText)};
+	xhr.open("POST",{% url 'lifetrack:occur' %});
+	xhr.setRequestHeader("X-CSRFToken",csrf);
+	xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	xhr.send(post);
+	}
