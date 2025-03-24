3f84463b7848af9eeb0499e527194f861a53245d
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-24 18:01:40 +0000

    Missed one
    dammit
    
    Though this should happen much quicker than you can select multiple things,
    so it shouldn't matter in practice.
    But still, better safe than sorry
    
    Fwiw, csrf shouldn't actually be different between invocations so we could leave it,
    but purity I suppose

--- static/js.js
+++ static/js.js
@@ -1,8 +1,8 @@
 function showReg(){document.getElementById("user_form").style.display="block";}
 function tickBox(e){
 	var cb=e.target;dt=cb.parentElement.id;var hb=cb.parentElement.parentElement.parentElement.id;var ls=cb.parentElement.parentElement.parentElement.parentElement.id;
-	csrf=document.getElementById("csrf").firstChild.value;
-	post="dt="+encodeURIComponent(dt)+"&hb="+encodeURIComponent(hb)+"&ls="+encodeURIComponent(ls);
+	var csrf=document.getElementById("csrf").firstChild.value;
+	var post="dt="+encodeURIComponent(dt)+"&hb="+encodeURIComponent(hb)+"&ls="+encodeURIComponent(ls);
 	cb.disabled=true
 	var xhr=new XMLHttpRequest();
 	xhr.onreadystatechange=function(){if(this.readyState==4){cb.disabled=false;if(this.status==200)alert(xhr.responseText)}};
