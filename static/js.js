1ae85f90aad4b9a50eec310a4a7fc44b8f7526fa
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-24 19:36:51 +0000

    Add extra POST param for toggle direction

--- static/js.js
+++ static/js.js
@@ -2,7 +2,7 @@ function showReg(){document.getElementById("user_form").style.display="block";}
 function tickBox(e){
 	var cb=e.target;dt=cb.parentElement.id;var hb=cb.parentElement.parentElement.parentElement.id;var ls=cb.parentElement.parentElement.parentElement.parentElement.id;
 	var csrf=document.getElementById("csrf").firstChild.value;
-	var post="dt="+encodeURIComponent(dt)+"&hb="+encodeURIComponent(hb)+"&ls="+encodeURIComponent(ls);
+	var post="dt="+encodeURIComponent(dt)+"&hb="+encodeURIComponent(hb)+"&ls="+encodeURIComponent(ls)+"&set="+(cb.checked?"y":"n");
 	cb.disabled=true
 	var xhr=new XMLHttpRequest();
 	xhr.onreadystatechange=function(){if(this.readyState==4){cb.disabled=false;if(this.status==200)alert(xhr.responseText)}};
