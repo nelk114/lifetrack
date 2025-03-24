2a2d630d19c2046afaf812a5ba3b8a68156c904c
Bernhard Nicolás Ersfeld Mandujano <2202252e@gla.ac.uk>
2025-03-24 17:57:34 +0000

    Disable checkboxes during processing
    Needs the previous fix to avoid a race condition
    if selecting multiple things takes less time
    than a server round‐trip

--- static/js.js
+++ static/js.js
@@ -3,9 +3,9 @@ function tickBox(e){
 	var cb=e.target;dt=cb.parentElement.id;var hb=cb.parentElement.parentElement.parentElement.id;var ls=cb.parentElement.parentElement.parentElement.parentElement.id;
 	csrf=document.getElementById("csrf").firstChild.value;
 	post="dt="+encodeURIComponent(dt)+"&hb="+encodeURIComponent(hb)+"&ls="+encodeURIComponent(ls);
-	//alert(post);
+	cb.disabled=true
 	var xhr=new XMLHttpRequest();
-	xhr.onreadystatechange=function(){if(this.readyState==4&&this.status==200)alert(xhr.responseText)};
+	xhr.onreadystatechange=function(){if(this.readyState==4){cb.disabled=false;if(this.status==200)alert(xhr.responseText)}};
 	xhr.open("POST","/occur/");
 	xhr.setRequestHeader("X-CSRFToken",csrf);
 	xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
